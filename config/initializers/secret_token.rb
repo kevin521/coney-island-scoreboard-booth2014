7037160bb83dd0a588682eca96ce60b512aa1ccb
Chen Kevin <kwchen@andrew.cmu.edu>
2014-04-09 11:20:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ConeyIslandScoreboard::Application.config.secret_key_base = 'f283f67dc6eb84827bb9926def0b81aeb0f86634a9a44397cf40c7a87edbbf2d6de20d8cb8362ef734904a5397f5b01c0bb8353ba2356ab6f79449a1d2612744'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ConeyIslandScoreboard::Application.config.session_store :cookie_store, key: '_coney_island_scoreboard_session'
