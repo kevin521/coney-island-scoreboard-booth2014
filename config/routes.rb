aa72dfe586d60919f3cfd380a83bd6a9886d1cb1
Chen Kevin <kwchen@andrew.cmu.edu>
2014-04-09 12:08:00 -0400

    Added some basic css stylings/bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ConeyIslandScoreboard::Application.routes.draw do
   resources :scores
 
+    root to: 'scores#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
